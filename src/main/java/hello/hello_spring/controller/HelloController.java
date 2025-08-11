739f3da965ffa47caa317d4d0be55ea99ba7a622
bucheasal <leejunwoo9@gmail.com>
2025-08-11 16:46:40 +0900

    회원가입(?)

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,54 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    //mvc, 템플릿 엔진
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //API
+    @GetMapping("hello-string")
+    @ResponseBody //view 필요 없고 그대로 서버에 넘긴다
+    public String helloString (@RequestParam("name") String name){
+        return "hello "+name;
+    }
+
+    //json 방식
+    //객체가 반환되면 jsonconvertor  key : value
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
+
+
